04d02930bb9583ae61873d6cae64c1735a735d75
Mariajosechaves <61141542+Mariajosechaves@users.noreply.github.com>
2020-02-19 12:07:05 +0100

    proyecto principal.

---
+++ ejercicios/clase1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace MJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Declaración de variable de tipo integer
+            //// llamada "x".
+            //int x;
+            //// Inicialización de variable llamada "x"
+            //// con el valor 4.
+            //x = 4;
+            //// Declaración e inicialización.
+            //int y = 3;
+            //float z = 3.3f;
+            //double w = 3.52d;
+            //char letra = 'A';
+            //string frase = "Hola que tal";
+            //bool booleanFalso = false;
+            //bool boleanoVerdadero = true;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+            //Console.WriteLine(w);
+            //Console.WriteLine(letra);
+            //Console.WriteLine(frase);
+            //Console.WriteLine(booleanFalso);
+            //Console.WriteLine(boleanoVerdadero);
+
+            //Console.Write("Escriba el primer número: ");
+            //int n1 = int.Parse(Console.ReadLine());
+            //Console.Write("Escriba el segundo número: ");
+            //int n2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("La suma es: " + (n1 + n2));
+            //Console.WriteLine("La resta es: " + (n1 - n2));
+            //Console.WriteLine("La porcentaje es: " + (n1 % n2));
+            //Console.WriteLine("La división es: " + (n1 / n2));
+            //Console.WriteLine("La multiplicación es: " + (n1 * n2));
+
+            //Console.Write("Introduzca su edad: ");
+            //int age = int.Parse(Console.ReadLine());
+            //if (age >= 65)
+            //{
+            //    Console.WriteLine("Estas jubilado");
+            //}
+            //else if (age >= 18)
+            //{
+            //    Console.WriteLine("Eres mayor de edad");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eres menor de edad");
+            //}
+
+            //bool windy = false;
+            //bool rainy = true;
+            //bool sunny = true;
+
+            //if (!windy && (rainy || sunny))
+            //{
+            //    Console.WriteLine("Puedes coger el paraguas");
+            //}
+
+            Console.Write("Introduzca la nota: ");
+            int grade = int.Parse(Console.ReadLine());
+            if (grade <= 10 && grade >= 9)
+            {
+                Console.WriteLine("Sobresaliente");
+            }
+            else if (grade >= 7 && grade < 9)
+            {
+                Console.WriteLine("Notable");
+            }
+            else if (grade >= 6 && grade < 7)
+            {
+                Console.WriteLine("Bien");
+            }
+            else if (grade >= 5 && grade < 6)
+            {
+                Console.WriteLine("Suficiente");
+            }
+            else if (grade >= 0 && grade < 5)
+            {
+                Console.WriteLine("Insuficiente");
+            }
+            else
+            {
+                Console.WriteLine("Se ha equivocado");
+            }
+
+            Console.Write("Introduzca la nota: ");
+            int nota = int.Parse(Console.ReadLine());
+            switch (nota)
+            {
+                case 10:
+                case 9:
+                   Console.WriteLine("Sobresaliente");
+                    break;
+                case 8:
+                case 7:
+                    Console.WriteLine("Notable");
+                    break;
+                case 6:
+                    Console.WriteLine("Bien");
+                    break;
+                case 5:
+                    Console.WriteLine("Suficiente");
+                    break;
+                case 4:
+                case 3:
+                case 2:
+                case 1:
+                case 0:
+                    Console.WriteLine("Insuficiente");
+                    break;
+                default:
+                    Console.WriteLine("Te has equivocado");
+                    break;
+            }
+        } 
+    }
+}
---
+++ ejercicios/clase2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace clase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Introduce primer número: ");
+            //int numero1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Introduce segundo número: ");
+            //int numero2 = int.Parse(Console.ReadLine());
+            //if (numero1 == numero2)
+            //{
+            //    Console.WriteLine("Iguales");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Diferentes");
+            //    if (numero1 > numero2)
+            //    {
+            //        Console.WriteLine("Mayor" +numero1);
+            //    }
+            //    else
+            //    {
+            ////        Console.WriteLine("Mayor" +numero2);
+            ////    }
+
+            ////}
+            //Console.WriteLine("Introduce número: ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x % 2 == 0)
+            //{
+            //    Console.WriteLine("Número par");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Número impar");
+            //}
+
+            //Console.WriteLine("Introduce el primer número: ");
+            //int n1 = int.Parse(Console.ReadLine()); 
+            ////Console.WriteLine("Introduce el segundo número: ");
+            ////int n2 = int.Parse(Console.ReadLine());
+            ////if (n1 % n2 == 0)
+            ////{
+            ////    Console.WriteLine("Número múltiplo");
+            ////}
+            ////else
+            ////{
+            ////    Console.WriteLine("Número no múltiplo");
+            ////}
+
+            //Console.WriteLine("Introduce el número: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number == 0)
+            //{
+            //    Console.WriteLine("Cero");
+            //}
+            //else if (number > 0)
+            //{
+            //    Console.WriteLine("Positivo");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Negativo");
+            //}
+
+            //Console.WriteLine("Introduce 1,2 ó 3: ");
+            //int nmr = int.Parse(Console.ReadLine());
+            //switch (nmr)
+            //{
+            //    case 3:
+            //        Console.WriteLine("Capullo");
+            //            break;
+            //    case 2:
+            //        Console.WriteLine("¡Cómete el bocadillo de jamón! y jamón se quedó sin bocadillo");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("Al mal tiempo buena cara");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Error");
+            //        break;
+            //}
+            //Console.WriteLine("Introduce nota de matemáticas: ");
+            //double matemáticas = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Introduce nota de inglés: ");
+            //double inglés = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Introduce nota de historia: ");
+            //double historia = int.Parse(Console.ReadLine());
+            //Console.Write("Tu nota media es: " + (matemáticas + inglés + historia) / 3);
+
+            Console.WriteLine("Introduce tu altura: ");
+            double altura = int.Parse(Console.ReadLine());
+            if (altura >= 135 && altura <= 140)
+            {
+                Console.WriteLine("Eres un pitufo");
+            }
+            else if (altura > 140 && altura <= 150)
+            {
+                Console.WriteLine("Eres un minion");
+            }
+            else if (altura > 150 && altura <= 160)
+            {
+                Console.WriteLine("Eres un hobbit");
+            }
+            else
+            {
+                Console.WriteLine("Estas ideal");
+            }
+        }
+    }
+}
